93d9b43c710faf78bc63f2d6e6f5032ca8f040f0
BartekBurzynski <bartlomiej.burzynski@wp.pl>
2019-05-24 16:13:13 +0200

    Wprowadzanie liczby

--- src/GiveNumber.java
+++ src/GiveNumber.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class GiveNumber {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Wprowadź liczbę54");
+        System.out.println("Wprowadź liczbę");
         int i = 0;
 
         while (true) {
             i = reader.nextInt();
-            if (i == 1) {
-                break;
-            }
+
+            
             if (i < 100) {
                 System.out.println("zbyt mała liczba");
             } else if (i > 200) {
@@ -19,6 +18,7 @@ public class GiveNumber {
                 System.out.println("liczba nie dzieli się przez 3");
             } else {
                 System.out.println("Podana liczba jest ok");
+                break;
             }
         }
     }
